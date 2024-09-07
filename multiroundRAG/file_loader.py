529506503d755bd5209898faee05c93460a5a7ef
shivvor2 <marklooskyfall@gmail.com>
2024-09-07 13:45:05 +0800

    Added option customize doc_id generation (via callable) for store_and_embed_documents()

--- multiroundRAG/file_loader.py
+++ multiroundRAG/file_loader.py
@@ -5,6 +5,7 @@ from .chunking import sentence_level_chunking
 from typing import Callable, List, Union
 from torch import Tensor
 from numpy import ndarray
+from xxhash import xxh128_intdigest
 
 def read_file(file_path):
     # Read a file using Unstructured library.
@@ -29,15 +30,20 @@ def read_directory(directory_path, recursive=True):
 
     return documents
 
-# Can customize doc_id later
-def store_and_embed_documents(documents: list, collection: Collection, embedding_func: Callable, chunker_kwargs: dict = dict(), batch_encoding: bool = False):
+# Example usage for doc_ig_gen_func
+# import xxhash
+# <Some Code>
+# store_and_embed_documents(documents, collection, doc_id_gen_func = xxhash.xxh128_hexdigest)
+
+def store_and_embed_documents(documents: list, collection: Collection, embedding_func: Callable, chunking_func: Callable = sentence_level_chunking, doc_id_gen_func: Union[Callable, None] = xxh128_intdigest, batch_encoding: bool = False):
     for i, doc in enumerate(documents):
-        index_document(doc, collection, embedding_func, i, chunker_kwargs, batch_encoding)
+        doc_id = doc_id_gen_func(doc) if doc_id_gen_func else i
+        index_document(doc, collection, embedding_func, chunking_func, doc_id, batch_encoding)
 
 # use partial to create an embedding function "embedder" that eats 1 arguement only and returns an embedding (str -> torch.tensor (or other equivalent class))
-def index_document(document, collection: Collection, embedding_func: Callable, doc_id: int, chunker_kwargs: dict = dict(), batch_encoding: bool = False):
+def index_document(document, collection: Collection, embedding_func: Callable, chunking_func: Callable, doc_id: int, batch_encoding: bool = False):
     data = []
-    chunks = sentence_level_chunking(document, **chunker_kwargs) # returns list of dicts with fields: "text" and "chunk_length"
+    chunks = chunking_func(document) # returns list of dicts with fields: "text" and "chunk_length"
     embeddings_all = embedding_func(chunks) if batch_encoding else None
     for i, chunk in enumerate(chunks): # Should we consider making this it's own function?
         embedding = embeddings_all[i] if embeddings_all else embedding_func(chunk["text"])
