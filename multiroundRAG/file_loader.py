a0d0bd54d8c57188aea73e6050588d2bada7ee47
shivvor2 <marklooskyfall@gmail.com>
2024-09-07 13:21:52 +0800

    Added batch_encoding option in index_document

--- multiroundRAG/file_loader.py
+++ multiroundRAG/file_loader.py
@@ -2,7 +2,9 @@ import os
 from unstructured.partition.auto import partition
 from pymilvus import Collection
 from .chunking import sentence_level_chunking
-from typing import Callable
+from typing import Callable, List, Union
+from torch import Tensor
+from numpy import ndarray
 
 def read_file(file_path):
     # Read a file using Unstructured library.
@@ -28,16 +30,17 @@ def read_directory(directory_path, recursive=True):
     return documents
 
 # Can customize doc_id later
-def store_and_embed_documents(documents: list, collection: Collection, embedding_func: Callable, chunker_kwargs: dict = dict()):
+def store_and_embed_documents(documents: list, collection: Collection, embedding_func: Callable, chunker_kwargs: dict = dict(), batch_encoding: bool = False):
     for i, doc in enumerate(documents):
-        index_document(doc, collection, embedding_func, i, chunker_kwargs)
+        index_document(doc, collection, embedding_func, i, chunker_kwargs, batch_encoding)
 
 # use partial to create an embedding function "embedder" that eats 1 arguement only and returns an embedding (str -> torch.tensor (or other equivalent class))
-def index_document(document, collection: Collection, embedding_func: Callable, doc_id: int, chunker_kwargs: dict = dict()):
+def index_document(document, collection: Collection, embedding_func: Callable, doc_id: int, chunker_kwargs: dict = dict(), batch_encoding: bool = False):
     data = []
     chunks = sentence_level_chunking(document, **chunker_kwargs) # returns list of dicts with fields: "text" and "chunk_length"
+    embeddings_all = embedding_func(chunks) if batch_encoding else None
     for i, chunk in enumerate(chunks): # Should we consider making this it's own function?
-        embedding = embedding_func(chunk["text"])
+        embedding = embeddings_all[i] if embeddings_all else embedding_func(chunk["text"])
         entity_dict = {
             "document_id": doc_id,
             "chunk_id": i,
@@ -48,4 +51,3 @@ def index_document(document, collection: Collection, embedding_func: Callable, d
         data.append(entity_dict)
     collection.insert(data)
     # collection.flush()  # might need to flush in production
-
